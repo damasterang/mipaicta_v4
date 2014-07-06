90dc7217220c66991c690629f3f3e2f03f9ffed0
damasterang <damasterang@outlook.com>
2014-07-06 14:01:14 +0700

    siang hari mendung panas

---
+++ connect.php
@@ -0,0 +1,11 @@
+<?php
+
+	$host = "localhost";
+	$username = "root";
+	$password = "abcd";
+	$database = "u254057372_semar";
+
+	mysql_connect($host,$username,$password);
+	mysql_select_db($database);
+
+?>
